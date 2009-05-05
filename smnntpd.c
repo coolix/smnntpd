70234548c461bbde8dff1a64ca0e1a4a5f3dca39
coolix <david.guedj@gmail.com>
2009-05-05 22:11:31 +0200

    The daemon now daemonize() itself

--- smnntpd.c
+++ smnntpd.c
@@ -3,6 +3,7 @@
 #include <string.h>		/* memset*/
 #include <sys/socket.h>		/* socket*/
 #include <sys/types.h>		/* compatibilité BSD*/
+#include <sys/stat.h>
 #include <stdio.h>		/* perror, fopen, fileno*/
 #include <stdlib.h>		/* exit*/
 #include <unistd.h>		/* close*/
@@ -12,12 +13,14 @@
 #include <errno.h>
 #include <confuse.h>
 #include <sys/select.h>
+#include <syslog.h>
 
-#define LISTENQ  32
-#define NNTP_BUF 512
-#define max(a, b) ((a) > (b) ? (a) : (b))
+#define DAEMON_NAME	"smNNTPd"
+#define LISTENQ		32
+#define NNTP_BUF	512
+#define max(a, b)	((a) > (b) ? (a) : (b))
 #define PROXY_ADDR	cfg_getstr(cfg, "proxy_addr")
-#define PROXY_PORT	cfg_getint(cfg, "proxy_port")
+#define PROXY_PORT	atoi(cfg_getstr(cfg, "proxy_port"))
 #define SERVER_ADDR	cfg_getstr(cfg, "server_addr")
 #define SERVER_PORT	cfg_getstr(cfg, "server_port")
 #define USERNAME	cfg_getstr(cfg, "username")
@@ -114,7 +117,7 @@ cfg_t *read_conf()
 	cfg_t *cfg;
 	cfg_opt_t opts[] = {
 		CFG_STR("proxy_addr", "127.0.0.1", CFGF_NONE),
-		CFG_INT("proxy_port", 119, CFGF_NONE),
+		CFG_STR("proxy_port", "119", CFGF_NONE),
 		CFG_STR("server_addr", NULL, CFGF_NONE),
 		CFG_STR("server_port", NULL, CFGF_NONE),
 		CFG_STR("username", NULL, CFGF_NONE),
@@ -158,6 +161,33 @@ int start_proxy(cfg_t *cfg)
 	return proxyfd;
 }
 
+static void daemonize()
+{
+	pid_t pid;
+
+	if (getppid() == 1) return;
+	
+	pid = fork();
+	
+	if (pid < 0)
+		die("fork");
+
+	if (pid > 0)
+		exit(EXIT_SUCCESS);
+	
+	umask(0222);
+
+	if (setsid() < 0)
+		die("setsid()");
+
+	if (chdir("/") < 0)
+		die("chdir");
+
+	freopen("/dev/null", "r", stdin);
+	freopen("/dev/null", "w", stdout);
+	freopen("/dev/null", "w", stderr);
+}
+
 int main(void)
 {
 	struct sockaddr client;
@@ -173,6 +203,11 @@ int main(void)
 	sa.sa_handler = sigchld_handler;
 	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = SA_RESTART;
+
+	openlog(DAEMON_NAME, LOG_PID, LOG_LOCAL5);
+	syslog(LOG_INFO, "starting");
+	daemonize();
+
 	if (sigaction(SIGCHLD, &sa, NULL) == -1)
 		die("sigaction");
 
@@ -181,65 +216,68 @@ int main(void)
 			die("accept");
 
 		/* Here comes a new challenger, let's fork() about sex */
-		if (!fork()) {
-			FILE *server_stream, *client_stream;
-			int serverfd;
-			char srv_msg[NNTP_BUF];
-			struct timeval timeout = { 0, 1000 };
-			fd_set readfds, readfds_m;
-
-			close(proxyfd);
-			server_stream = server_connect(SERVER_ADDR, SERVER_PORT);
-			serverfd = fileno(server_stream);
-
-			if ((client_stream = fdopen(clientfd, "r+")) == NULL)
-				die("fdopen");
-
-			setlinebuf(server_stream);
-			setlinebuf(client_stream);
-
-			if (AUTH) {
-				fgets(srv_msg, NNTP_BUF, server_stream);
-				if (set_credential(server_stream,
-						   USERNAME,
-						   PASSWORD
-				    )
-				    ) {
-					fprintf(stderr, "couldn't set creds\n");
-					exit(EXIT_FAILURE);
-				}
-				fputs(srv_msg, client_stream);
-			}
-
-			setsockopt(clientfd, SOL_SOCKET, SO_RCVTIMEO,
-				   &timeout, sizeof timeout);
-			setsockopt(serverfd, SOL_SOCKET, SO_RCVTIMEO,
-				   &timeout, sizeof timeout);
-
-			FD_ZERO(&readfds);
-			FD_SET(clientfd, &readfds);
-			FD_SET(serverfd, &readfds);
-
-			for (;;) {
-				readfds_m = readfds;
-				select(max(serverfd, clientfd) + 1,
-				       &readfds_m, NULL, NULL, NULL);
-				if (FD_ISSET(serverfd, &readfds_m))
-					if (proxy_transfert
-					    ('S', server_stream, client_stream))
-						break;
-				if (FD_ISSET(clientfd, &readfds_m)) {
-					if (proxy_transfert
-					    ('C', client_stream, server_stream))
-						break;
-				}
-			}
-			fclose(server_stream);
-			fclose(client_stream);
-			exit(0);
-		} else {
+		if (fork()) {
 			close(clientfd);
+			continue;
 		}
+
+		/* Child */
+		FILE *server_stream, *client_stream;
+		int serverfd;
+		char srv_msg[NNTP_BUF];
+		struct timeval timeout = { 0, 1000 };
+		fd_set readfds, readfds_m;
+
+		close(proxyfd);
+		server_stream = server_connect(SERVER_ADDR, SERVER_PORT);
+		serverfd = fileno(server_stream);
+
+		if ((client_stream = fdopen(clientfd, "r+")) == NULL)
+			die("fdopen");
+
+		setlinebuf(server_stream);
+		setlinebuf(client_stream);
+
+		if (AUTH) {
+			fgets(srv_msg, NNTP_BUF, server_stream);
+			if (set_credential(server_stream,
+					   USERNAME,
+					   PASSWORD
+			    )
+			    ) {
+				fprintf(stderr, "couldn't set creds\n");
+				exit(EXIT_FAILURE);
+			}
+			fputs(srv_msg, client_stream);
+		}
+
+		setsockopt(clientfd, SOL_SOCKET, SO_RCVTIMEO,
+			   &timeout, sizeof timeout);
+		setsockopt(serverfd, SOL_SOCKET, SO_RCVTIMEO,
+			   &timeout, sizeof timeout);
+
+		FD_ZERO(&readfds);
+		FD_SET(clientfd, &readfds);
+		FD_SET(serverfd, &readfds);
+
+		for (;;) {
+			readfds_m = readfds;
+			select(max(serverfd, clientfd) + 1,
+			       &readfds_m, NULL, NULL, NULL);
+			if (FD_ISSET(serverfd, &readfds_m))
+				if (proxy_transfert
+				    ('S', server_stream, client_stream))
+					break;
+			if (FD_ISSET(clientfd, &readfds_m)) {
+				if (proxy_transfert
+				    ('C', client_stream, server_stream))
+					break;
+			}
+		}
+		fclose(server_stream);
+		fclose(client_stream);
+		exit(0);
 	}
+	syslog(LOG_NOTICE, "terminated");
 	exit(EXIT_SUCCESS);
 }
